2bf0b76cd817a42d973d506e9a5409457f0285de
mervyn20010949 <85254322+mervyn20010949@users.noreply.github.com>
2021-06-03 02:45:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Linh ơi anh bảo cái này!',
+    introDesc: `con gà và quả trứng em chọn gà hay chọn anh`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnYes: 'Vem chọn anh huhuhuhu',
+    btnNo: 'Không, Anh trai à, em chọn gà :3',
+    question:'Trên thế giới hơn 7 tỉ con gà mà sao em lại chọn anh ?',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    reply: 'chọn thì chọn không chọn thì thôi',
+    mess: 'Anh biết mà thể nào em cũng bỏ qua con gà để chọn anh :)))',
+    messDesc: 'thôi tôi nay tắm rửa sạch sẽ nằm sẵn trên giường chờ anh qua :))',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
