8c26f190b1f5dac05dbaf5eed5b163a0fff3433c
mervyn20010949 <85254322+mervyn20010949@users.noreply.github.com>
2021-06-03 10:22:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: Linh ơi anh bảo linh nàyyyy',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    Đằng sau chữ yêu đây là thương... Linh sinh nhật vui vẻ nè`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
